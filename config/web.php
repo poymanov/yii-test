eba378034ac5a76b918212ea74c7ffb33ffd2a47
Nikolay <n.poymanov@gmail.com>
2016-05-11 22:29:53 +0300

    [new] add pretty urls
    Включено отображение ЧПУ

--- config/web.php
+++ config/web.php
@@ -37,15 +37,15 @@ $config = [
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
-        /*
+        'db' => require(__DIR__ . '/db.php'),        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
